0bde765d41078e334b4bdaeeeb44b2138bd5cc8d
Dongyeon Park <plzfday@gmail.com>
2022-10-23 23:32:04 +0100

    Edit `grep.py`

--- src/applications/grep.py
+++ src/applications/grep.py
@@ -4,12 +4,16 @@ import re
 
 class Grep(Application):
     def exec(self, args, out):
-        if len(args) != 2:
+        if len(args) < 2:
             raise ValueError("wrong number of command line arguments")
         pattern = args[0]
-        file = args[1]
-        with open(file) as f:
-            lines = f.readlines()
-            for line in lines:
-                if re.search(pattern, line):
-                    out.append(line)
+        files = args[1:]
+        for file in files:
+            with open(file) as f:
+                lines = f.readlines()
+                for line in lines:
+                    if re.match(pattern, line):
+                        if len(files) > 1:
+                            out.append(f"{file}:{line}")
+                        else:
+                            out.append(line)
