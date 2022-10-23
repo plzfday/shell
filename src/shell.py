b6df6f8b752a186b5281ea52b154b56f554f8145
youngwoohome <youngwoo5662@gmail.com>
2022-10-23 23:43:30 +0100

    Add feature for non specified files for cat, head, tail and grep

--- src/shell.py
+++ src/shell.py
@@ -5,6 +5,15 @@ from os import listdir
 from collections import deque
 from glob import glob
 
+def stdinput():
+    sin = sys.stdin
+    sout = sys.stdout
+    while True:
+        try:
+            s = sin.readline()
+            sout.write(s)
+        except KeyboardInterrupt:
+            break
 
 def eval(cmdline, out):
     raw_commands = []
@@ -44,56 +53,79 @@ def eval(cmdline, out):
                 if not f.startswith("."):
                     out.append(f + "\n")
         elif app == "cat":
-            for a in args:
-                with open(a) as f:
-                    out.append(f.read())
+            if len(args) != 0:
+                for a in args:
+                    with open(a) as f:
+                        out.append(f.read())
+            else:
+                stdinput()
         elif app == "head":
-            if len(args) != 1 and len(args) != 3:
-                raise ValueError("wrong number of command line arguments")
-            if len(args) == 1:
-                num_lines = 10
-                file = args[0]
-            if len(args) == 3:
-                if args[0] != "-n":
-                    raise ValueError("wrong flags")
-                else:
-                    num_lines = int(args[1])
-                    file = args[2]
-            with open(file) as f:
-                lines = f.readlines()
-                for i in range(0, min(len(lines), num_lines)):
-                    out.append(lines[i])
-        elif app == "tail":
-            if len(args) != 1 and len(args) != 3:
-                raise ValueError("wrong number of command line arguments")
-            if len(args) == 1:
-                num_lines = 10
-                file = args[0]
-            if len(args) == 3:
-                if args[0] != "-n":
-                    raise ValueError("wrong flags")
-                else:
-                    num_lines = int(args[1])
-                    file = args[2]
-            with open(file) as f:
-                lines = f.readlines()
-                display_length = min(len(lines), num_lines)
-                for i in range(0, display_length):
-                    out.append(lines[len(lines) - display_length + i])
-        elif app == "grep":
-            if len(args) < 2:
-                raise ValueError("wrong number of command line arguments")
-            pattern = args[0]
-            files = args[1:]
-            for file in files:
+            if len(args) == 2:
+                stdinput()
+            else:
+                if len(args) != 1 and len(args) != 3:
+                    raise ValueError("wrong number of command line arguments")
+                if len(args) == 1:
+                    num_lines = 10
+                    file = args[0]
+                if len(args) == 3:
+                    if args[0] != "-n":
+                        raise ValueError("wrong flags")
+                    else:
+                        num_lines = int(args[1])
+                        file = args[2]
                 with open(file) as f:
                     lines = f.readlines()
-                    for line in lines:
-                        if re.match(pattern, line):
-                            if len(files) > 1:
-                                out.append(f"{file}:{line}")
-                            else:
-                                out.append(line)
+                    for i in range(0, min(len(lines), num_lines)):
+                        out.append(lines[i])
+        elif app == "tail":
+            if len(args) == 2:
+                stdinput()
+            else:
+                if len(args) != 1 and len(args) != 3:
+                    raise ValueError("wrong number of command line arguments")
+                if len(args) == 1:
+                    num_lines = 10
+                    file = args[0]
+                if len(args) == 3:
+                    if args[0] != "-n":
+                        raise ValueError("wrong flags")
+                    else:
+                        num_lines = int(args[1])
+                        file = args[2]
+                with open(file) as f:
+                    lines = f.readlines()
+                    display_length = min(len(lines), num_lines)
+                    for i in range(0, display_length):
+                        out.append(lines[len(lines) - display_length + i])
+        elif app == "grep":
+            if len(args) == 1:
+                pattern = args[0]
+                sin = sys.stdin
+                sout = sys.stdout
+                while True:
+                    try:
+                        s = sin.readline()
+                        if pattern in s:
+                            sout.write(s)
+                        else:
+                            sout.write(s)
+                    except KeyboardInterrupt:
+                        break
+            elif len(args) < 2:
+                raise ValueError("wrong number of command line arguments")
+            elif len(args) == 2:
+                pattern = args[0]
+                files = args[1:]
+                for file in files:
+                    with open(file) as f:
+                        lines = f.readlines()
+                        for line in lines:
+                            if re.match(pattern, line):
+                                if len(files) > 1:
+                                    out.append(f"{file}:{line}")
+                                else:
+                                    out.append(line)
         else:
             raise ValueError(f"unsupported application {app}")
 
