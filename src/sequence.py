6daab6c9042fdee561bc0128f550b95ec660b90c
Dongyeon Park <plzfday@gmail.com>
2022-11-18 17:21:24 +0000

    feat: new compilation of applications and `sort`

---
+++ src/sequence.py
@@ -0,0 +1,9 @@
+import command
+
+
+class Sequence(command.Command):
+    def __init__(self):
+        pass
+
+    def eval(self, in_stream, out_stream):
+        pass
--- test/test_shell.py
+++ test/test_shell.py
@@ -1,15 +1,34 @@
 import unittest
 
-from shell import eval
 from collections import deque
 
 
-class TestShell(unittest.TestCase):
-    def test_shell(self):
-        out = deque()
-        eval("echo foo", out)
-        self.assertEqual(out.popleft(), "foo\n")
-        self.assertEqual(len(out), 0)
+class TestSort(unittest.TestCase):
+    def setUp(self):
+        from src.apps import Sort
+
+        self.app = Sort()
+        self.in_stream = deque()
+        self.out_stream = deque()
+        self.sample_in = ["bat", "abc", " apple", " Abc", "BALL", "ABc", "bat"]
+        self.sample_default_out = [" Abc", " apple", "ABc", "BALL", "abc", "bat", "bat"]
+
+        with open("test/test_sort.txt", "w") as f:
+            for line in self.sample_in:
+                f.write(line + "\n")
+
+    def test_sort(self):
+        self.app.exec(["test/test_sort.txt"], self.in_stream, self.out_stream)
+        self.assertEqual(self.out_stream, deque(self.sample_default_out))
+
+    def test_sort_reverse(self):
+        self.app.exec(["-r", "test/test_sort.txt"], self.in_stream, self.out_stream)
+        self.assertEqual(self.out_stream, deque(self.sample_default_out[::-1]))
+
+    def test_sort_stdin(self):
+        self.in_stream.extend(self.sample_in)
+        self.app.exec([], self.in_stream, self.out_stream)
+        self.assertEqual(self.out_stream, deque(self.sample_default_out))
 
 
 if __name__ == "__main__":
