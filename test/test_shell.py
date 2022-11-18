1194d95e037571f0b50b1177d9d175c63e702eb8
Dongyeon Park <plzfday@gmail.com>
2022-11-18 20:26:08 +0000

    fix: fix `TestSort` unit test

--- test/test_shell.py
+++ test/test_shell.py
@@ -10,25 +10,28 @@ class TestSort(unittest.TestCase):
         self.app = Sort()
         self.in_stream = deque()
         self.out_stream = deque()
+
         self.sample_in = ["bat", "abc", " apple", " Abc", "BALL", "ABc", "bat"]
-        self.sample_default_out = [" Abc", " apple", "ABc", "BALL", "abc", "bat", "bat"]
+        self.sample_in = [x + "\n" for x in self.sample_in]
+        self.sample_out = [" Abc", " apple", "ABc", "BALL", "abc", "bat", "bat"]
+        self.sample_out = [x + "\n" for x in self.sample_out]
 
         with open("test/test_sort.txt", "w") as f:
             for line in self.sample_in:
-                f.write(line + "\n")
+                f.write(line)
 
     def test_sort(self):
         self.app.exec(["test/test_sort.txt"], self.in_stream, self.out_stream)
-        self.assertEqual(self.out_stream, deque(self.sample_default_out))
+        self.assertEqual(self.out_stream, deque(self.sample_out))
 
     def test_sort_reverse(self):
         self.app.exec(["-r", "test/test_sort.txt"], self.in_stream, self.out_stream)
-        self.assertEqual(self.out_stream, deque(self.sample_default_out[::-1]))
+        self.assertEqual(self.out_stream, deque(self.sample_out[::-1]))
 
     def test_sort_stdin(self):
         self.in_stream.extend(self.sample_in)
         self.app.exec([], self.in_stream, self.out_stream)
-        self.assertEqual(self.out_stream, deque(self.sample_default_out))
+        self.assertEqual(self.out_stream, deque(self.sample_out))
 
 
 if __name__ == "__main__":
