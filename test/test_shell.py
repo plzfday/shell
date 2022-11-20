bdfee62583556d4158c2996b2c4960adca528883
youngwoohome <youngwoo5662@gmail.com>
2022-11-20 23:28:46 +0000

    re : without comment

--- test/test_shell.py
+++ test/test_shell.py
@@ -44,71 +44,71 @@ class TestGrep(unittest.TestCase):
 
 
 
-# class TestUniq(unittest.TestCase):
-#     def setUp(self):
-#         from src.apps import Uniq
+class TestUniq(unittest.TestCase):
+    def setUp(self):
+        from src.apps import Uniq
         
-#         self.app = Uniq()
-#         self.in_stream = deque()
-#         self.out_stream = deque()
-#         self.sample_in = ["abc", "ABC", "Abc", "bat", " Ball"]
-#         self.sample_default_out = ["abc", "ABC", "Abc", "bat", " Ball"]
-#         #-i included
-#         self.sample_caseNotSensitive_out = ["abc", "bat", " Ball"]
+        self.app = Uniq()
+        self.in_stream = deque()
+        self.out_stream = deque()
+        self.sample_in = ["abc", "ABC", "Abc", "bat", " Ball"]
+        self.sample_default_out = ["abc", "ABC", "Abc", "bat", " Ball"]
+        #-i included
+        self.sample_caseNotSensitive_out = ["abc", "bat", " Ball"]
 
-#         with open("test/test_uniq.txt", "w") as f:
-#             for line in self.sample_in:
-#                 f.write(line + "\n")
+        with open("test/test_uniq.txt", "w") as f:
+            for line in self.sample_in:
+                f.write(line + "\n")
                 
 
-#     def test_uniq(self):
-#         self.app.exec(["test/test_uniq.txt"], self.in_stream, self.out_stream)
-#         self.assertEqual(self.out_stream, deque(self.sample_default_out))
+    def test_uniq(self):
+        self.app.exec(["test/test_uniq.txt"], self.in_stream, self.out_stream)
+        self.assertEqual(self.out_stream, deque(self.sample_default_out))
 
-#     def test_uniq_caseNotSensitive(self):
-#         self.app.exec(["-i","test/test_uniq.txt"], self.in_stream, self.out_stream)
-#         self.assertEqual(self.out_stream, deque(self.sample_caseNotSensitive_out))
+    def test_uniq_caseNotSensitive(self):
+        self.app.exec(["-i","test/test_uniq.txt"], self.in_stream, self.out_stream)
+        self.assertEqual(self.out_stream, deque(self.sample_caseNotSensitive_out))
     
-#     def test_uniq_stdin(self):
-#         self.in_stream.extend(self.sample_in)
-#         self.app.exec([], self.in_stream, self.out_stream)
-#         self.assertEqual(self.out_stream, deque(self.sample_default_out))
+    def test_uniq_stdin(self):
+        self.in_stream.extend(self.sample_in)
+        self.app.exec([], self.in_stream, self.out_stream)
+        self.assertEqual(self.out_stream, deque(self.sample_default_out))
 
-#     def test_uniq_stdin_with_option(self):
-#         self.in_stream.extend(self.sample_in)
-#         self.app.exec(["-i"], self.in_stream, self.out_stream)
-#         self.assertEqual(self.out_stream, deque(self.sample_caseNotSensitive_out))
+    def test_uniq_stdin_with_option(self):
+        self.in_stream.extend(self.sample_in)
+        self.app.exec(["-i"], self.in_stream, self.out_stream)
+        self.assertEqual(self.out_stream, deque(self.sample_caseNotSensitive_out))
     
 
-# class TestSort(unittest.TestCase):
-#     def setUp(self):
-#         from src.apps import Sort
+class TestSort(unittest.TestCase):
+    def setUp(self):
+        from src.apps import Sort
 
-#         self.app = Sort()
-#         self.in_stream = deque()
-#         self.out_stream = deque()
+        self.app = Sort()
+        self.in_stream = deque()
+        self.out_stream = deque()
 
-#         self.sample_in = ["bat", "abc", " apple", " Abc", "BALL", "ABc", "bat"]
-#         self.sample_in = [x + "\n" for x in self.sample_in]
-#         self.sample_out = [" Abc", " apple", "ABc", "BALL", "abc", "bat", "bat"]
-#         self.sample_out = [x + "\n" for x in self.sample_out]
+        self.sample_in = ["bat", "abc", " apple", " Abc", "BALL", "ABc", "bat"]
+        self.sample_in = [x + "\n" for x in self.sample_in]
+        self.sample_out = [" Abc", " apple", "ABc", "BALL", "abc", "bat", "bat"]
+        self.sample_out = [x + "\n" for x in self.sample_out]
 
-#         with open("test/test_sort.txt", "w") as f:
-#             for line in self.sample_in:
-#                 f.write(line)
+        with open("test/test_sort.txt", "w") as f:
+            for line in self.sample_in:
+                f.write(line)
 
-#     def test_sort(self):
-#         self.app.exec(["test/test_sort.txt"], self.in_stream, self.out_stream)
-#         self.assertEqual(self.out_stream, deque(self.sample_out))
+    def test_sort(self):
+        self.app.exec(["test/test_sort.txt"], self.in_stream, self.out_stream)
+        self.assertEqual(self.out_stream, deque(self.sample_out))
 
-#     def test_sort_reverse(self):
-#         self.app.exec(["-r", "test/test_sort.txt"], self.in_stream, self.out_stream)
-#         self.assertEqual(self.out_stream, deque(self.sample_out[::-1]))
+    def test_sort_reverse(self):
+        self.app.exec(["-r", "test/test_sort.txt"], self.in_stream, self.out_stream)
+        self.assertEqual(self.out_stream, deque(self.sample_out[::-1]))
 
-#     def test_sort_stdin(self):
-#         self.in_stream.extend(self.sample_in)
-#         self.app.exec([], self.in_stream, self.out_stream)
-#         self.assertEqual(self.out_stream, deque(self.sample_out))
+    def test_sort_stdin(self):
+        self.in_stream.extend(self.sample_in)
+        self.app.exec([], self.in_stream, self.out_stream)
+        self.assertEqual(self.out_stream, deque(self.sample_out))
 
 
 if __name__ == "__main__":
